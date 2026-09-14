462394ed5e875c97b6eee0b5a9445cf56d5c3cce
Adham Amin Rashid Abu Hajar <abuhager360@gmail.com>
2026-09-15 00:19:07 +0300

    Update portfolio sitemap domain

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://my-portfolio-five-azure-69.vercel.app/sitemap.xml',
+    sitemap: 'https://adhamabuhagerdev.site/sitemap.xml',
   };
 }
