8eefedf13348e4fa9b1f625b9c000fd212b56ab5
Adham Amin Rashid Abu Hajar <abuhager360@gmail.com>
2026-09-20 18:27:49 +0300

    Use transparent AH monogram in portfolio navigation

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
       <nav aria-label="Site" className="page-shell site-nav">
         <a href="#top" className="brand" aria-label="Adham Abu Hager, return to portfolio overview">
           <span className="brand__logo" aria-hidden="true">
-            <Image src="/brand/ah-mark.webp" alt="" width={264} height={240} sizes="52px" priority />
+            <Image src="/brand/ah-logo-light.svg" alt="" width={372} height={242} sizes="60px" priority />
           </span>
           <span className="brand__copy">
             <strong>Adham Abu Hager</strong>
